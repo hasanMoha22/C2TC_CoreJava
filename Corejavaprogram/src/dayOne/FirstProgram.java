e665747aea23d7347c2073e86fe8ad277f6d931d
hasanMoha22 <mhasanmoha22@gmail.com>
2025-10-03 20:24:41 +0530

    Learned a new things

---
+++ Corejavaprogram/src/dayOne/FirstProgram.java
@@ -0,0 +1,9 @@
+package dayOne;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Corejavaprogram/src/dayOne/IdentifiersDemo.java
@@ -0,0 +1,13 @@
+package dayOne;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="AjithKumar"; //use camelcase
+		System.out.println("value is : "+ studentName);
+	}
+
+}
