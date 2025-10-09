e6fb7f73d2cc483534efbb7f4d4322fdd2ee2b9b
hasanMoha22 <mhasanmoha22@gmail.com>
2025-10-09 07:38:00 +0530

    Day by Day Learning new things

---
+++ Corejavaprogram/src/com/tnsif/dayseven/overriding/v1/ICICI.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayseven.overriding.v1;
+
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
---
+++ Corejavaprogram/src/com/tnsif/dayseven/overriding/v1/RBI.java
@@ -0,0 +1,8 @@
+package com.tnsif.dayseven.overriding.v1;
+
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
